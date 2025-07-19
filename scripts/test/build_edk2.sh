686d850a9b9f6e71de618e6c84b31a4983d8cd82
hanbings <hanbings@hanbings.io>
2025-07-20 05:16:53 +0800

    chore(github actions): add github actions build script.

--- scripts/test/build_edk2.sh
+++ scripts/test/build_edk2.sh
@@ -7,28 +7,33 @@ EDK_DIR="$WORKSPACE_DIR/edk2"
 
 mkdir -p "$WORKSPACE_DIR"
 
-echo "[1/3] 克隆 EDK2 仓库..."
-if [ ! -d "$EDK_DIR" ]; then
-    git clone --recurse-submodule https://github.com/tianocore/edk2.git "$EDK_DIR"
-else
-    echo "EDK2 仓库已存在，跳过克隆。"
-fi
-
-echo "[2/3] 准备 EDK2 构建环境..."
+echo "[1/3] 准备 EDK2 构建环境..."
 cd "$WORKSPACE_DIR"
 
 if [ ! -d "ToolChain/RISCV" ]; then
     echo "下载 RISCV 工具链..."
     mkdir -p ToolChain/RISCV
-    wget https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2025.07.16/riscv64-elf-ubuntu-24.04-gcc-nightly-2025.07.16-nightly.tar.xz
+    wget -q https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/2025.07.16/riscv64-elf-ubuntu-24.04-gcc-nightly-2025.07.16-nightly.tar.xz
     tar -xf riscv64-elf-ubuntu-24.04-gcc-nightly-2025.07.16-nightly.tar.xz -C "${WORKSPACE_DIR}/ToolChain/RISCV"
 else
     echo "RISCV 工具链已存在，跳过下载。"
 fi
 
+if ! command -v python &> /dev/null; then
+    echo "Python3 未安装，请先安装 Python3。"
+    exit 1
+fi
+
 if ! command -v python3 &> /dev/null; then
     echo "Python3 未安装，请先安装 Python3。"
     exit 1
+fi
+
+echo "[2/3] 克隆 EDK2 仓库..."
+if [ ! -d "$EDK_DIR" ]; then
+    git clone --recurse-submodule https://github.com/tianocore/edk2.git "$EDK_DIR"
+else
+    echo "EDK2 仓库已存在，跳过克隆。"
 fi
 
 export PATH="$WORKSPACE_DIR/ToolChain/RISCV/riscv/bin:$PATH"
