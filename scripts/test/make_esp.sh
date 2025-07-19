3abae48ecd1e05cccf6a18b63202dce4bfeff412
hanbings <hanbings@hanbings.io>
2025-07-20 06:31:06 +0800

    chore(github actions): add github actions build script.

---
+++ scripts/test/make_esp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+PROJECT_ROOT=$(pwd)
+IMG_NAME="fat32_disk_test.img"
+MOUNT_DIR="mnt_fat32"
+ESP_DIR="$MOUNT_DIR/EFI/BOOT"
+
+if [ ! -d "$MOUNT_DIR" ]; then
+    mkdir "$MOUNT_DIR"
+fi
+
+echo "[1/3] 挂载 FAT32 镜像..."
+sudo mount -o loop "$IMG_NAME" "$MOUNT_DIR"
+
+echo "[2/3] 创建 ESP 目录结构..."
+mkdir -p "$ESP_DIR"
+
+echo "[3/3] 复制 efi 文件到 ESP..."
+cp "$PROJECT_ROOT/edk2/edk2/Build/MdeModule/DEBUG_GCC5/RISCV64/Hello/Hello/DEBUG/Hello.efi" "$ESP_DIR/BOOTRISCV64.EFI"
